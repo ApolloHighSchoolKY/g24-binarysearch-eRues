33eefa11a19d1953f18df25c999f2706cd456430
eRues <ellie.rue@stu.daviess.kyschools.us>
2025-04-16 14:05:16 +0000

    completed

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,29 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		int minDex = 0;
+		int maxDex = ray.length - 1;
+		int midDex = 0;
+		Arrays.sort(ray);
 
+		while(minDex <= maxDex)
+		{
+			midDex = (minDex + maxDex) /2;
+			if(ray[midDex] > item)
+			{
+				maxDex = midDex - 1;
+			}
+			else if(ray[midDex] < item)
+			{
+				minDex = midDex + 1;
+			}
+			else
+			{
+				return midDex;
+			}
+		}
+
+		return - 1;
+		
 	}
 }
